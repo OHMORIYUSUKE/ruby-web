926759508621165b1ced7e0fa42ed4659dcfedc0
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2024-01-17 16:51:26 +0900

    feat: Server Actionに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,130 +1,131 @@
-"use client";
-import axios, { AxiosError } from "axios";
-import { useEffect, useState } from "react";
-import CodeEditor from "@uiw/react-textarea-code-editor";
+import { redirect } from "next/navigation";
+import fs from "fs/promises";
+import { DefaultRubyVM } from "@ruby/wasm-wasi/dist/node";
+import { RbError } from "@ruby/wasm-wasi";
 
-export default function Home() {
-  const [code, setCode] = useState(`user_input = gets.chomp
-output_message = user_input + " Ruby !"
-puts output_message
-`);
-  const [input, setInput] = useState("Hello");
-  const [result, setResult] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+export default function Home({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const output = searchParams.output;
+  const exitCode = searchParams.exit_code;
 
-  const handleSubmit = async () => {
-    try {
-      const response = await axios.post("/api/ruby/execute", { code, input });
-      setErrorMessage("");
-      setResult(response.data);
-    } catch (error) {
-      setResult("");
-      if (axios.isAxiosError(error)) {
-        setErrorMessage((error as AxiosError).response?.data as string);
-      } else {
-        setErrorMessage("ネットワークで問題が発生しました。");
-      }
-    }
+  const example = {
+    code: `user_input = gets.chomp
+
+greeting = "Hello #{user_input}"
+  
+puts greeting
+  `,
+    input: "World !",
   };
 
-  const [vmInfo, setVmInfo] = useState("");
+  const runCode = async (formData: FormData) => {
+    "use server";
 
-  useEffect(() => {
-    // GETリクエストを送信する関数
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/ruby/info");
-        const result = await response.json();
-        setVmInfo(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
+    let result = "";
+    let exitCode = 0;
+
+    const binary = await fs.readFile(
+      process.cwd() + "/node_modules/@ruby/3.3-wasm-wasi/dist/ruby.wasm"
+    );
+    const modules = await WebAssembly.compile(binary);
+    const { vm } = await DefaultRubyVM(modules);
+
+    try {
+      // 通常コンソールに出力する内容をメモリ上の文字列に書き込む
+      vm.eval(`
+        require "stringio"
+        $stdout = $stderr = StringIO.new(+"", "w")
+      `);
+
+      // ユーザーからの標準入力テキスト
+      const input = formData.get("input");
+
+      // ユーザーからのコードテキスト
+      const code = formData.get("code") as string;
+
+      // ユーザーからの標準入力テキストをrubyに渡す
+      vm.eval(`
+        require "stringio"
+        $stdin = StringIO.new("${input}")
+      `);
+      // 実行
+      vm.eval(code);
+      // メモリ上の標準出力を取得
+      const output = vm.eval(`$stdout.string`).toString();
+
+      console.log("Ruby VM Result:" + output);
+      result = output;
+    } catch (e) {
+      console.log("Ruby VM Result:", (e as RbError).toString());
+      result = (e as RbError).toString();
+      exitCode = 1;
+    }
+    redirect(
+      `/?output=${encodeURIComponent(result)}&exit_code=${encodeURIComponent(
+        exitCode
+      )}`
+    );
+  };
 
   return (
-    <div style={{ margin: "0 auto", width: "50%" }}>
-      <div style={{ textAlign: "center" }}>
-        <code style={{ fontSize: "large" }}>💎 Ruby Playground</code>
-      </div>
-      <br />
-      <code>コード</code>
-      <CodeEditor
-        value={code}
-        language="rb"
-        onChange={(evn) => setCode(evn.target.value)}
-        padding={15}
-        style={{
-          backgroundColor: "#f5f5f5",
-          fontFamily:
-            "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-        }}
-      />
-      <br />
-      <code>標準入力</code>
-      <CodeEditor
-        value={input}
-        language="rb"
-        onChange={(evn) => setInput(evn.target.value)}
-        padding={15}
-        style={{
-          backgroundColor: "#f5f5f5",
-          fontFamily:
-            "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-        }}
-      />
-      <br />
-      <div style={{ textAlign: "center" }}>
-        <button style={{ width: "50%" }} onClick={handleSubmit}>
-          実行
-        </button>
-      </div>
-      <br />
-      {result !== "" ? (
-        <CodeEditor
-          value={result}
-          padding={15}
-          readOnly={true}
+    <>
+      <div style={{ margin: "0 auto", width: "50%" }}>
+        <div style={{ textAlign: "center" }}>
+          <code style={{ fontSize: "large" }}>💎 Ruby Playground</code>
+        </div>
+        <br />
+        <form action={runCode}>
+          <code>コード</code>
+          <br />
+          <textarea
+            name="code"
+            rows={10}
+            cols={70}
+            defaultValue={example.code}
+            style={{ width: "100%" }}
+          ></textarea>
+          <br />
+          <code>標準入力</code>
+          <br />
+          <textarea
+            name="input"
+            rows={3}
+            cols={70}
+            defaultValue={example.input}
+            style={{ width: "100%" }}
+          ></textarea>
+          <br />
+          <div style={{ textAlign: "center" }}>
+            <input
+              style={{ width: "50%", marginTop: "10px" }}
+              id="submit"
+              type="submit"
+              value="実行"
+            />
+          </div>
+        </form>
+        <br />
+        <div
           style={{
-            backgroundColor: "#000",
-            fontFamily:
-              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-            color: "white",
-          }}
-        />
-      ) : (
-        <></>
-      )}
-      {errorMessage !== "" ? (
-        <CodeEditor
-          value={errorMessage}
-          padding={15}
-          readOnly={true}
-          style={{
-            backgroundColor: "#000",
-            fontFamily:
-              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-            color: "red",
-          }}
-        />
-      ) : (
-        <></>
-      )}
-      <div style={{ position: "fixed", bottom: 0, width: "50%" }}>
-        <CodeEditor
-          value={vmInfo}
-          language="rb"
-          readOnly={true}
-          padding={15}
-          style={{
-            backgroundColor: "#f5f5f5",
-            fontFamily:
-              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            wordBreak: "break-word",
             whiteSpace: "pre-wrap",
+            width: "100%",
+            backgroundColor: "black",
+            padding: "10px",
           }}
-        />
+        >
+          {Number(exitCode) === 0 ? (
+            <code style={{ color: "white" }}>{output}</code>
+          ) : (
+            <code style={{ color: "red" }}>{output}</code>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
